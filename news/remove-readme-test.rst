37588280daa8d5d98eb3747b703eeef9f500ad8c
Sangjoon Bob Lee <bobleesj@gmail.com>
2025-04-18 11:53:19 -0400

    tests: improve existing tests with group's best practices

---
+++ news/remove-readme-test.rst
@@ -0,0 +1,23 @@
+**Added:**
+
+* <news item>
+
+**Changed:**
+
+* <news item>
+
+**Deprecated:**
+
+* <news item>
+
+**Removed:**
+
+* Remove requirements/README.txt containing instructions for listing dependencies. since they are already provided in the official documentation.
+
+**Fixed:**
+
+* <news item>
+
+**Security:**
+
+* <news item>
--- {{ cookiecutter.github_repo_name }}/tests/test_calculator.py
+++ {{ cookiecutter.github_repo_name }}/tests/test_calculator.py
@@ -1,17 +1,35 @@
+import pytest
 from {{cookiecutter.package_dir_name}} import calculator
 
 
 def test_dot_product_2D():
-    """Test the dot product function in 2D."""
+    """Test the dot product function with 2D vectors."""
     a = [1, 2]
     b = [3, 4]
     expected = 11
-    assert calculator.dot_product(a, b) == expected
+    actual = calculator.dot_product(a, b)
+    assert actual == expected
 
 
 def test_dot_product_3D():
-    """Test the dot product function in 3D."""
+    """Test the dot product function with 3D vectors."""
     a = [1, 2, 3]
     b = [4, 5, 6]
     expected = 32
-    assert calculator.dot_product(a, b) == expected
+    actual = calculator.dot_product(a, b)
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "a, b, expected",
+    [
+        # Test whether the doc product function works with different vector sizes
+        # 2D vectors, expect 11
+        ([1, 2], [3, 4], 11),
+        # 3D vectors, exppect 32
+        ([1, 2, 3], [4, 5, 6], 32),
+    ]
+)
+def test_dot_product(a, b, expected):
+    actual = calculator.dot_product(a, b)
+    assert actual == expected
