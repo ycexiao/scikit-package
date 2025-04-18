f2e3b755eac4fe6fba81df4474178792d917745f
Sangjoon Bob Lee <bobleesj@gmail.com>
2025-04-18 13:06:00 -0400

    chore: implement docstrings and test best practices for the doc_product example

--- {{ cookiecutter.github_repo_name }}/src/{{ cookiecutter.package_dir_name }}/calculator.py
+++ {{ cookiecutter.github_repo_name }}/src/{{ cookiecutter.package_dir_name }}/calculator.py
@@ -2,5 +2,42 @@ import numpy as np
 
 
 def dot_product(a, b):
-    """Calculate the dot product of two vectors."""
-    return np.dot(a, b)
+    """Compute the dot product of two vectors of any size.
+
+    Ensure that the inputs of a and b are of the same size.
+    The supported types are Python list, Python tuple, and NumyPy array.
+
+    Parameters
+    ----------
+    a : array_like
+        The first input of vector.
+    b : array_like
+        The second input vector.
+
+    Returns
+    -------
+    float
+        The dot product of the two vectors.
+
+    Examples
+    --------
+
+    Compute the dot product of two lists:
+    >>> a = [1, 2, 3]
+    >>> b = [4, 5, 6]
+    >>> dot_product(a, b)
+    32.0
+
+    Compute the dot product of two tuples
+    >>> a = (1, 2, 3)
+    >>> b = (4, 5, 6)
+    >>> dot_product(a, b)
+    32.0
+
+    Compute the dot product of two numpy arrays:
+    >>> a = np.array([1, 2, 3])
+    >>> b = np.array([4, 5, 6])
+    >>> dot_product(a, b)
+    32.0
+    """
+    return float(np.dot(a, b))
