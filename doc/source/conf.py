66966542bacf9e456d8e738dc6734128a3081fa3
Caden Myers <cjm2304@columbia.edu>
2025-05-05 14:49:05 -0400

    edit conf.py to point to _static

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -139,6 +139,7 @@ html_theme = "sphinx_rtd_theme"
 #
 html_theme_options = {
     "navigation_with_keys": "true",
+    "logo_only": True,
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
@@ -158,7 +159,7 @@ html_theme_options = {
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-# html_favicon = None
+html_favicon = "_static/scikit-package-logo-box.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -166,7 +167,7 @@ html_theme_options = {
 # html_static_path = ['_static']
 
 html_static_path = ["_static"]
-html_logo = "_static/scikit-package-logo-text.png"
+html_logo = "_static/scikit-package-logo-docs.png"
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
