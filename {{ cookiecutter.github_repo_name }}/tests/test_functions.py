cdba5e394f00d5a1f606122f106a8af71bd3dfa6
Caden Myers <cjm2304@columbia.edu>
2025-06-05 17:46:42 -0400

    bug: fix spacing in pr template and black edit in test_function.py

--- {{ cookiecutter.github_repo_name }}/tests/test_functions.py
+++ {{ cookiecutter.github_repo_name }}/tests/test_functions.py
@@ -31,8 +31,7 @@ def test_dot_product_3D_list():
         # C3: numpy arrays, expect correct float output
         (np.array([1, 2]), np.array([3, 4]), 11.0),
         (np.array([1, 2, 3]), np.array([4, 5, 6]), 32.0),
-    ]
-
+    ],
 )
 def test_dot_product(a, b, expected):
     actual = functions.dot_product(a, b)
