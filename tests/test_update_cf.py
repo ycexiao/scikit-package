76ff5b0931103ea9e39178774f1a873f161c7fc8
Sangjoon Bob Lee <sl5400@columbia.edu>
2025-06-14 14:20:47 +0900

    refactor: compare the output of updated meta explicitly

--- tests/test_update_cf.py
+++ tests/test_update_cf.py
@@ -16,14 +16,18 @@ source:
 """
     new_version = "1.0.6"
     new_sha256 = "123456789abcdef0123456789"
+    expected_updated_meta = """{%- set version = "1.0.6" -%}
+package:
+  name: {{ name|lower }}
+  version: {{ version }}
+
+source:
+  url: https://pypi.org/packages/source/{{ name[0] }}/{{ name }}/{{ name }}-{{ version }}.tar.gz  # noqa: E501
+  sha256: 123456789abcdef0123456789
+"""
     with tempfile.TemporaryDirectory() as tmpdir:
         meta_file = Path(tmpdir) / "meta.yaml"
         meta_file.write_text(original_meta)
         _update_meta_yaml(str(meta_file), new_version, new_sha256)
-        updated_lines = meta_file.read_text().splitlines()
-        assert updated_lines[0] == '{%- set version = "1.0.6" -%}'
-        assert updated_lines[1] == "package:"
-        assert updated_lines[2] == "  name: {{ name|lower }}"
-        assert updated_lines[3] == "  version: {{ version }}"
-        assert updated_lines[4] == ""
-        assert updated_lines[7] == "  sha256: 123456789abcdef0123456789"
+        updated_meta = meta_file.read_text()
+        assert updated_meta == expected_updated_meta
