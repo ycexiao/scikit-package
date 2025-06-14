24ee2c2cbe6dd85861cfd6d216d6dddf48ea530f
Sangjoon Bob Lee <sl5400@columbia.edu>
2025-06-14 16:30:57 +0900

    chore: fix pre-commit

--- src/scikit_package/app.py
+++ src/scikit_package/app.py
@@ -83,7 +83,7 @@ def setup_subparsers(parser):
     build_commands = [
         (
             "api-doc",
-            "Generate API docs in doc/source/api for namespace import packages.",
+            "Generate API in doc/source/api for namespace import package.",
         ),
     ]
     _add_subcommands(subparsers_build, build_commands, api_doc.build)
