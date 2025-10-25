cc2f162e3b25716f6d03178df84f9655319262e3
Yuchen Ethan Xiao <yx2924@columbia.edu>
2025-10-25 13:16:57 -0400

    test: add test implemenetation

--- tests/test_gh.py
+++ tests/test_gh.py
@@ -1,12 +1,47 @@
+import pytest
+
 from scikit_package.cli.gh import _get_issue_content  # noqa: F401
 
 
-def test_get_issue_content():
+def test_get_issue_content(get_issue_mocker):
     # C1: a valid issue url. Expect the source_repo_url and
     #   issue content are returned.
-    assert False
+    issue_url = (
+        "https://githun.com/user-or-orgname/reponame/issues/issue-number"
+    )
+    expected_issue_content = {"title": "issue-title", "body": "issue-body"}
+    expected_source_repo_url = "https://github.com/user-or-orgname/reponame"
+    actual_source_repo_url, actual_issue_content = _get_issue_content(
+        issue_url
+    )
+    get_issue_mocker.assert_called_once()
+    assert actual_source_repo_url == expected_source_repo_url
+    assert actual_issue_content == expected_issue_content
 
 
-def test_get_issue_content_bad():
-    # C1: a not valid issue url. Expect ValueError.
-    assert False
+def test_get_issue_content_bad(get_issue_fail_mocker):
+    # C1: a not valid url. Expect ValueError.
+    issue_url = "non-valid-url"
+    with pytest.raises(
+        ValueError,
+        match=(
+            f"{issue_url} is not a valid url to be parsed. "
+            "Please input the url of the issue to be broadcasted. "
+            "Its format should be https://"
+            "github.com/username/reponame/issues/issue-number"
+        ),
+    ):
+        source_repo_url, issue_content = _get_issue_content(issue_url)
+    # C2: a valid url but can not find the corresponding issue.
+    #   Expect ValueError.
+    issue_url = "https://github.com/nonexisting/nonexisting/issues/0"
+    with pytest.raises(
+        ValueError,
+        match=(
+            f"Can not find the corresponding issue from {issue_url}. "
+            "Please ensure the input url is correct. "
+            "Its format should be https://"
+            "github.com/username/reponame/issues/issue-number"
+        ),
+    ):
+        source_repo_url, issue_content = _get_issue_content(issue_url)
