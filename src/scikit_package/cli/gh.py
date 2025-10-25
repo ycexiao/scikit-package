acf016779ee990b122bb6d3c3fb6d094cde6060c
Yuchen Ethan Xiao <yx2924@columbia.edu>
2025-10-24 20:34:54 -0400

    chore: add the function signature and docstring

---
+++ src/scikit_package/cli/gh.py
@@ -0,0 +1,16 @@
+def _broadcast_issue_to_urls(issue_content, broadcast_urls):
+    """Broadcast issue to the repos pointed by the urls in the
+    broadcast_urls.
+
+    Parameters
+    ----------
+    issue_content : dict
+        issue_content needed to create issues in each repo.
+    broadcast_urls : list of str
+        urls to the target repos.
+
+    Returns
+    -------
+    None
+    """
+    return
