da0dc0b1a909e2411258e819005052665d11deae
Yuchen Ethan Xiao <yx2924@columbia.edu>
2025-10-24 20:27:17 -0400

    chore: add the function signature and docstring

---
+++ news/broadcast-get-broadcast-urls.rst
@@ -0,0 +1,23 @@
+**Added:**
+
+* No news added: Chore: develop _get_broadcast_urls required for package broadcast <issue-url>
+
+**Changed:**
+
+* <news item>
+
+**Deprecated:**
+
+* <news item>
+
+**Removed:**
+
+* <news item>
+
+**Fixed:**
+
+* <news item>
+
+**Security:**
+
+* <news item>
---
+++ src/scikit_package/cli/gh.py
@@ -0,0 +1,18 @@
+def _get_broadcast_urls():
+    """Get repo urls to broadcast from the interactions with users.
+
+    This function interacts with the user in the following order:
+    1. Prompt for the location of files used to decipher the alias for
+    the target broadcast repo urls.
+    2. Print the recognizable name for each single repo and each
+    group of repos.
+    3. Prompt to let user type the name of target repos or groups of
+    repos to broadcast the issue.
+
+    Returns
+    -------
+    broadcast_urls : list of str
+        a list of repo urls to broadcast the issue.
+    """
+    broadcast_urls = []
+    return broadcast_urls
